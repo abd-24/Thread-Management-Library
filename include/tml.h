6ba75e83d0fbda31f84ac2c90aaca7feebfc3924
Sanjukta Biswas <sanjuktaabiswas@gmail.com>
2025-12-04 19:35:08 +0530

    Update tml.h

--- include/tml.h
+++ include/tml.h
@@ -1,16 +1,19 @@
 #ifndef TML_H
 #define TML_H
 
+#include <stddef.h>
+
 typedef void (*tml_task_fn)(void *arg);
 
-typedef struct tml_task {
+typedef struct {
     tml_task_fn fn;
     void *arg;
 } tml_task_t;
 
-int tml_init(int workers);
-void tml_submit(tml_task_t *task);
-void tml_submit_fn(tml_task_fn fn, void *arg);
+int  tml_init(int workers);
+int  tml_submit(const tml_task_t *task);
+int  tml_submit_fn(tml_task_fn fn, void *arg);
+void tml_wait_all(void);
 void tml_shutdown(void);
 
 #endif
