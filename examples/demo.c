d91b476f2501b8cbf6a55d3d6bb77d283c64c815
Sanjukta Biswas <sanjuktaabiswas@gmail.com>
2025-12-04 19:37:08 +0530

    Update demo.c

--- examples/demo.c
+++ examples/demo.c
@@ -10,15 +10,23 @@ void task_fn(void *arg) {
 }
 
 int main() {
-    tml_init(4);
+    if (tml_init(4) != 0)
+        return 1;
 
     for (int i = 0; i < 100; i++) {
         int *p = malloc(sizeof(int));
+        if (!p)
+            return 1;
+
         *p = i;
-        tml_submit_fn(task_fn, p);
+
+        if (tml_submit_fn(task_fn, p) != 0) {
+            free(p);
+            return 1;
+        }
     }
 
-    sleep(1);
+    tml_wait_all();
     tml_shutdown();
     return 0;
 }
