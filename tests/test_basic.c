c94ce28577158939b4d80916b4d9bc2efee9e320
Sanjukta Biswas <sanjuktaabiswas@gmail.com>
2025-12-04 19:30:10 +0530

    Update test_basic.c

--- tests/test_basic.c
+++ tests/test_basic.c
@@ -1,21 +1,29 @@
-// tests/test_basic.c
-#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "../include/tml.h"
 
 void task(void *arg) {
-    int *p = arg;
-    printf("task %d\n", *p);
+    int *p = (int *)arg;
+    free(p);
 }
 
 int main() {
-    tml_init(2);
-    for (int i=0;i<10;i++){
+    if (tml_init(2) != 0)
+        return 1;
+
+    for (int i = 0; i < 10; i++) {
         int *v = malloc(sizeof(int));
+        if (!v)
+            return 1;
         *v = i;
-        tml_submit_fn(task, v);
+
+        if (tml_submit_fn(task, v) != 0) {
+            free(v);
+            return 1;
+        }
     }
-    sleep(1);
+
+    tml_wait_all();
     tml_shutdown();
     return 0;
 }
